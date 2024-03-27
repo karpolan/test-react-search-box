d6fd3fd634742c3ae2d14ae88764297cbe752d23
Anton Karpenko <karpolan@karpolan.com>
2024-03-27 17:49:18 +0100

    feat: MuiView routing at /mui

---
+++ src/views/MuiSolution/MuiSolutionView.tsx
@@ -0,0 +1,17 @@
+import { AppView } from '../../components';
+import NotImplementedView from '../NotImplementedView';
+
+/**
+ * Renders "Welcome" view
+ * url: /mui
+ * @page Mui
+ */
+const MuiSolutionView = () => {
+  return (
+    <AppView>
+      <NotImplementedView />
+    </AppView>
+  );
+};
+
+export default MuiSolutionView;
